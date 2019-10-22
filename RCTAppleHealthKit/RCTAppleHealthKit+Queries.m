47b6a844af10033e4b342a53a213fe04d00f64f2
Martijn Reijerse <martijn@ivaldi.nl>
2019-10-22 17:56:44 +0800

    exclude manual steps for hkstatisticscollectionquery

--- RCTAppleHealthKit/RCTAppleHealthKit+Queries.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Queries.m
@@ -386,10 +386,14 @@
                                                      fromDate:[NSDate date]];
     anchorComponents.hour = 0;
     NSDate *anchorDate = [calendar dateFromComponents:anchorComponents];
+    
+    // Exclude manual added steps
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"metadata.%K != YES", HKMetadataKeyWasUserEntered];
+    
 
     // Create the query
     HKStatisticsCollectionQuery *query = [[HKStatisticsCollectionQuery alloc] initWithQuantityType:quantityType
-                                                                           quantitySamplePredicate:nil
+                                                                           quantitySamplePredicate:predicate
                                                                                            options:HKStatisticsOptionCumulativeSum
                                                                                         anchorDate:anchorDate
                                                                                 intervalComponents:interval];
